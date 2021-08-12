4516c0e9747274f9a6b544ad5065804a0d7b300d
olivrrrrr <oliverekwalla@gmail.com>
2021-08-12 18:11:01 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+An object orientated library project using HTML/CSS and JS. 
+
+I hope to style library cards and use the Web Storage API to store library inputs, in future. 
--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const button = document.querySelector('.newBook');
+const popupContainer = document.querySelector('.popup-container');
+const overlay = document.getElementById('overlay'); 
+const closeButton = document.querySelector('.close-button'); 
+const submit = document.getElementById('submit'); 
+
 let myLibrary = [];
 
 function Book(title, author, pages, read){
@@ -5,30 +11,45 @@ function Book(title, author, pages, read){
     this.author = author 
     this.pages = pages
     this.read = read
-    this.info = function(){
-      return `${title} ${author} ${pages} ${read}`
-    }
-  }
-
- 
-  function addBookToLibrary(){
-    // let newTitle = prompt('Book title ?')
-    // let newAuthor = prompt('Book author ?')
-    // let newPages = prompt('Book pages ?')
-    // let newRead = prompt('Book read ?')
-    myLibrary.push(new Book(newTitle, newAuthor, newPages, newRead))
-  }
-
-addBookToLibrary()
-
-function displayBook(){
-  myLibrary.forEach((book)=>{
-    const container = document.querySelector('.container'); 
-    const content = document.createElement('div'); 
-    content.textContent = book.info()
-    container.appendChild(content)
-  })
 }
 
+submit.addEventListener('click', ()=>{
+  addBookToLibrary(); 
+  displayBook();
+  popupContainer.classList.remove('active')
+  overlay.classList.remove('active')
 
-displayBook()
+})
+
+// This allows the pop-up to occur
+button.addEventListener('click', ()=>{
+popupContainer.classList.add('active')
+overlay.classList.add('active')
+})
+
+closeButton.addEventListener('click', ()=> {
+popupContainer.classList.remove('active')
+overlay.classList.remove('active')
+})
+
+function addBookToLibrary(){
+  let bookName = document.getElementById('bookname').value; 
+  let authorName = document.getElementById('authorname').value; 
+  let numberOfPages = document.getElementById('numberofpages').value; 
+  let read = document.getElementById('read').value; 
+  let newBook = new Book(bookName, authorName, numberOfPages, read)
+  myLibrary.push(newBook)
+}
+
+function displayBook(){
+    const container = document.querySelector('.container'); 
+    const content = document.createElement('div');
+    content.innerHTML = `
+    <div class='card'>
+      <h2>Book Title : ${myLibrary[myLibrary.length-1].title}</h2>
+      <h2>Author : ${myLibrary[myLibrary.length-1].author}</h2>
+      <h2>Pages : ${myLibrary[myLibrary.length-1].pages}</h2>
+      <h2>Read : ${myLibrary[myLibrary.length-1].read}</h2>
+  </div>`
+    container.appendChild(content)
+}
